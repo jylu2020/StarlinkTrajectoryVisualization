a9816a0d7e09a524dc08c90185ae5efa4404cfde
Margaret Lu <jiayuelu@usc.edu>
2020-07-28 11:10:50 -0700

    complete left panel

--- src/components/SatSetting.js
+++ src/components/SatSetting.js
@@ -1,31 +1,55 @@
+
 import React, {Component} from 'react';
-import {InputNumber} from 'antd';
+import {InputNumber, Button, Slider} from 'antd';
 
 class SatSetting extends Component {
     constructor(){
         super();
         this.state = {
             observerLat: 0,
-            observerLong: 0
+            observerLong: 0,
+            observerElevation: 0,
+            satAlt: 90,
+            duration: [0, 0],
+            isLoading: false
         }
-
     }
 
     onChangeLong = (value) => {
-        console.log('value ', value)
         this.setState({
             observerLong: value
         })
     }
 
     onChangeLat = (value) => {
-        console.log('value ', value)
         this.setState({
             observerLat: value
         })
     }
 
+    onChangeEle = (value) => {
+        this.setState({
+            observerElevation: value
+        })
+    }
+    onChangeAlt = (value) => {
+        this.setState({
+            satAlt: Number(90 - +value)
+        })
+    }
+
+    onDurationChange = (value) => {
+        this.setState({
+            duration: value
+        })
+    }
+
+    showSatellite = () => {
+        this.props.onShow(this.state);
+    }
+
     render() {
+        const durationMarkers = { 0: '0', 89: '90'};
         return (
             <div className="sat-setting">
                 <div className="loc-setting">
@@ -66,6 +90,43 @@ class SatSetting extends Component {
                             />
                         </div>
                     </div>
+                </div>
+
+                <div className="altitude-setting">
+                    <p className="setting-label">Restrictions</p>
+
+                    <div>
+                        <span>Show only satellites which are higher than <br /> altitude</span>
+                        <InputNumber
+                            min={0}
+                            max={90}
+                            defaultValue={0}
+                            style={{margin: "8px 2px 0"}}
+                            onChange={this.onChangeAlt} /> <span>degrees.</span>
+                    </div>
+                </div>
+
+                <div className="duration-setting">
+                    <p className="setting-label">Duration(sec)</p>
+
+                    <Slider
+                        className="duration-slider"
+                        range
+                        step={1}
+                        defaultValue={[0, 90]}
+                        min={0}
+                        max={90}
+                        marks={durationMarkers}
+                        onChange={this.onDurationChange}
+                    />
+                </div>
+
+                <div className="show-nearby">
+                    <Button
+                        className="show-nearby-btn"
+                        size="large"
+                        onClick={this.showSatellite}
+                    >Find Nearby Satellites</Button>
                 </div>
             </div>
         );
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const SAT_BASE_URL = "https://www.n2yo.com/rest/v1/satellite";
+export const SAT_API_KEY = "HLP53T-JYNG56-7VKDMB-4IAI";
+export const STARLINK_CATEGORY = "52";
+export const NEARBY_SATELLITE = `${SAT_BASE_URL}/above`;
